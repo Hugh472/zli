83894bc6235ac832d672cb5aa2d7aff03f120a6e
Thanasis Filippidis <aflpd@bu.edu>
2021-05-06 20:08:38 +0300

    Close open tunnels on logout [CWC-628] (#100)
    * Added logout listener which closes open tunnels when a logout is detected

--- src/handlers/ssh-proxy.handler.ts
+++ src/handlers/ssh-proxy.handler.ts
@@ -43,6 +43,13 @@ export async function sshProxyHandler(configService: ConfigService, logger: Logg
             ssmTunnelService.sendData(data);
         });
     }
+
+    configService.logoutDetected.subscribe(async () => {
+        logger.debug('Logged out by another zli instance. Terminating ssh tunnel');
+        process.stderr.write(`\nLogged out by another zli instance. Terminating ssh tunnel...\n`);
+        await ssmTunnelService.closeTunnel();
+        await cleanExit(0, logger);
+    });
 }
 
 export interface SshTunnelParameters {
